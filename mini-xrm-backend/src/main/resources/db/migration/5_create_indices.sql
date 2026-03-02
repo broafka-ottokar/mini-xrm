d77323a3030b6df31cde64f5fbe31ea7b1c38f63
Balazs Jagri <b.jagri@gmail.com>
2026-03-03 00:59:54 +0100

    backend: adding missing indices

---
+++ mini-xrm-backend/src/main/resources/db/migration/5_create_indices.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_activity_person_responsible ON activity (person_responsible);
+CREATE INDEX idx_activity_partner_id         ON activity (partner_id);
+CREATE INDEX idx_activity_deleted            ON activity (deleted);
